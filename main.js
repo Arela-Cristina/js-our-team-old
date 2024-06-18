ccf08a621a137670e81c98e81da30eef2c1af77a
Arela-Cristina <instagrandine@gmail.com>
2024-06-18 15:40:51 +0200

    Refact array team

--- main.js
+++ main.js
@@ -3,26 +3,32 @@
 
 const arrayTeam = [
     {
+        'picture': 'Photography',
         'name': 'Kyojuro Rengoku',
         'work': 'Pilastro delle Fiamme',
     },
     {
+        'picture': 'Photography',
         'name': 'Muichiro Tokito',
         'work': 'Pilastro della Nebbia',
     },
     {
+        'picture': 'Photography',
         'name': 'Mitsuri Kanroji',
         'work': "Pilastro dell'Amore",
     },
     {
+        'picture': 'Photography',
         'name': 'Giyu Tomioka',
         'work': "Pilastro del Acqua",
     },
     {
+        'picture': 'Photography',
         'name': 'Shinobu Kocho',
         'work': "Pilastro degli Insetti",
     },
     {
+        'picture': 'Photography',
         'name': 'Tengen Uzui',
         'work': "Pilastro del Suono",
     },
