93a71e49281f12112e9dc207ae88c0ca5e403a69
Arela-Cristina <instagrandine@gmail.com>
2024-06-18 15:39:30 +0200

    Add arrayTeam list

---
+++ main.js
@@ -0,0 +1,29 @@
+//Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+//Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+const arrayTeam = [
+    {
+        'name': 'Kyojuro Rengoku',
+        'work': 'Pilastro delle Fiamme',
+    },
+    {
+        'name': 'Muichiro Tokito',
+        'work': 'Pilastro della Nebbia',
+    },
+    {
+        'name': 'Mitsuri Kanroji',
+        'work': "Pilastro dell'Amore",
+    },
+    {
+        'name': 'Giyu Tomioka',
+        'work': "Pilastro del Acqua",
+    },
+    {
+        'name': 'Shinobu Kocho',
+        'work': "Pilastro degli Insetti",
+    },
+    {
+        'name': 'Tengen Uzui',
+        'work': "Pilastro del Suono",
+    },
+]
